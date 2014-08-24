8e2a056a50eb01e032752ba38cf3f6cb34b393de
galatInna <innagalat@Innas-MacBook-Air.local>
2014-08-25 09:07:50 +1000

    changes

--- cachematrix.R
+++ cachematrix.R
@@ -17,14 +17,15 @@ makeCacheMatrix = function(x=matrix()){
 #returned by makeCacheMatrix above. If the inverse has already been calculated 
 #(and the matrix has not changed), then the cachesolve 
 #should retrieve the inverse from the cache.
-cacheSolve <- function (w=list,...){
-    answer <- w$get_answer()
-    if(!is.null(answer)){
-        message('getting cached data')
-        return(answer)
+cacheSolve <- function (w=list,...){  #function takes a list 
+    answer <- w$get_answer() # go to makecacheMatrix and check if there is any results available
+    if(!is.null(answer)){ #if we have answer (not NULL)
+        message('getting cached data') #print a text showing 'getting...'
+        return(answer) # and showing answer
     }
-    initial_data <- w$get_original_data()
-    answer <-solve(initial_data)
-    w$set_assign_answer(answer)
-    answer
+    initial_data <- w$get_original_data() # if we don't have answer yet
+    answer <-solve(initial_data) # go to makeCacheMatrix function and ask solve function 
+    # to solve a result on initial data
+    w$set_assign_answer(answer) # write/set a result to answer using makeCacheMatrix
+    answer #print the result
 }
